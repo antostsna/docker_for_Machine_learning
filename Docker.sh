53a6879e58e7bd2d5e743a745f45b3a816c44587
anto112 <m07158031@o365.mcut.edu.tw>
2019-06-25 18:35:16 +0800

    add file permision

--- Docker.sh
+++ Docker.sh
@@ -3,4 +3,5 @@
 echo $DISPLAY
 xhost +
 
-docker run --privileged=true -it --net=host -e DISPLAY --volume /tmp/.x11-unix -v ~/Moil_docker:/home/student/workdir oil:1.1 /bin/bash
+touch exemple.txt
+docker run --privileged=true -it --net=host -e DISPLAY --volume /tmp/.x11-unix -v ~/Documents:/home/student/workdir oil:1.1 /bin/bash
--- setting_env/add_user.sh
+++ setting_env/add_user.sh
@@ -4,4 +4,9 @@ for i in $( cat users.txt ); do
     adduser --force-badname $i --gecos "$i,RoomNumber,WorkPhone,HomePhone" --disabled-password
     echo "$i:123456" | chpasswd
     setfacl -m user:$i:rw /var/run/docker.sock
+    chmod 700 /home/$i
+    mkdir /home/$i/workdir
+    chmod 777 /home/$i/workdir/
+    cd /home/$i/workdir/
+    echo Wellcome $i, hope enjoy your learning. >>example.txt
 done
