3bf45fbc69bb3d47c1bcb94dc537a86f4b02a624
anto112 <m07158031@o365.mcut.edu.tw>
2019-06-25 20:56:15 +0800

    share bash to run

--- setting_env/add_user.sh
+++ setting_env/add_user.sh
@@ -4,9 +4,15 @@ for i in $( cat users.txt ); do
     adduser --force-badname $i --gecos "$i,RoomNumber,WorkPhone,HomePhone" --disabled-password
     echo "$i:123456" | chpasswd
     setfacl -m user:$i:rw /var/run/docker.sock
-    chmod 700 /home/$i
+    cp run.sh /home/$i
+    chmod 777 /home/$i/run.sh
     mkdir /home/$i/workdir
-    chmod 777 /home/$i/workdir/
     cd /home/$i/workdir/
-    echo Wellcome $i, hope enjoy your learning. >>example.txt
+    echo Wellcome $i, hope you enjoy the lecture. Fell free to ask some question when you get problem. Thank You !!!! >>Readme.txt
+    mkdir /home/$i/workdir/Documents
+    mkdir /home/$i/workdir/Data
+    chmod 700 /home/$i
+    chmod -R 777 /home/$i/workdir/
+    chmod 764 /home/$i/workdir/Readme.txt
+    echo "user $i created !!!"
 done
---
+++ setting_env/run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo $DISPLAY
+xhost +
+
+touch exemple.txt
+docker run --privileged=true -it --net=host -e DISPLAY --volume /tmp/.x11-unix -v ~/workdir:/home/student/workdir oil:1.1 /bin/bash
