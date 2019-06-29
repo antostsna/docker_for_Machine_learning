0fae0d703a869d5b716a0eb33d197664780ed17c
anto112 <m07158031@o365.mcut.edu.tw>
2019-06-29 12:31:07 +0800

    upload xrdp

---
+++ Xrdp_setting/xrdp.sh
@@ -0,0 +1,9 @@
+apt-get update	
+apt-get install xrdp gnome-tweak-tool gnome-shell-extension-*
+apt-get install xserver-xorg-core
+apt-get -y install xserver-xorg-input-all
+apt-get install xorgxrdp
+systemctl enable xrdp
+
+
+
---
+++ setting_env/Readme.txt
@@ -0,0 +1,1 @@
+Wellcome abc to MOIL system, hope you enjoy the lecture. Fell free to ask some question when you get problem. Thank You !!!!
