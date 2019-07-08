ace3738911140e1bef8e2f133a7f91023dd7931f
haryanto <47322969+anto112@users.noreply.github.com>
2019-07-08 10:08:29 +0800

    hide run.sh

--- setting_env/add_user.sh
+++ setting_env/add_user.sh
@@ -5,7 +5,8 @@ cp run.sh ~/
 for i in $( cat users.txt ); do
     adduser --force-badname $i --gecos "$i,RoomNumber,WorkPhone,HomePhone" --disabled-password
     echo "$i:123456" | chpasswd
-    echo /home/$i | xargs -n 1 cp -n ~/run.sh 
+    echo /home/$i | xargs -n 1 cp -n ~/run.sh
+    mv /home/$i/run.sh /home/$i/.run.sh
     setfacl -m user:$i:rw /var/run/docker.sock
     mkdir /home/$i/workdir
     cd /home/$i/workdir/
