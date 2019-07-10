15fa96af7c9edda150595f0e770dcbf93921df71
haryanto <47322969+anto112@users.noreply.github.com>
2019-07-11 00:47:18 +0800

    version 1.2.1

--- Docker.sh
+++ Docker.sh
@@ -4,4 +4,4 @@ echo $DISPLAY
 xhost +
 clear
 
-docker run --privileged=true -it --net=host -e DISPLAY --volume /tmp/.x11-unix -v ~/workdir:/home/student/workdir oil:1.1 /bin/bash
+docker run --privileged=true -it --net=host -e DISPLAY --volume /tmp/.x11-unix -v ~/workdir:/home/sudoer/ oil:1.1 /bin/bash
